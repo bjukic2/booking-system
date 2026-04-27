0ed18c4489de8bbf4be9c1a2ef73a416bd2d0e60
Bruno Jukić <brunojukic68@gmail.com>
2026-04-27 12:27:35 +0200

    Renamed middleware to proxy

---
+++ proxy.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export function proxy(req: Request) {
+  const host = req.headers.get("host") ?? "";
+  const sub = host.split(".")[0];
+
+  const res = NextResponse.next();
+
+  if (!isNaN(Number(sub))) {
+    res.headers.set("x-clinic-id", sub);
+  }
+
+  return res;
+}
