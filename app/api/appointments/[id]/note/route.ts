35a4237f6b827db1b41000f50a4049766f05026f
Bruno Jukić <brunojukic68@gmail.com>
2026-05-06 13:19:10 +0200

    Added option to add notes after the appointment is reserved

---
+++ app/api/appointments/[id]/note/route.ts
@@ -0,0 +1,29 @@
+import { appointmentService } from "@/backend/modules/appointments/appointment.service";
+import { ClinicRequest } from "@/types/next";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: ClinicRequest,
+  context: { params: Promise<{ id: string }> },
+) {
+  const { id } = await context.params;
+  try {
+    const clinicId = Number(req.headers.get("x-clinic-id"));
+
+    const body = await req.json();
+    const note = body.note ?? null;
+
+    const updated = await appointmentService.updateAppointmentNote(
+      Number(id),
+      clinicId,
+      note,
+    );
+
+    return NextResponse.json(updated);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return NextResponse.json({ error: err.message }, { status: 400 });
+    }
+    return NextResponse.json({ error: "Unknown error" }, { status: 500 });
+  }
+}
